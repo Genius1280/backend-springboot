458e2dc661d3fedbc7a3485d486ebdd70e1c6172
Genius1280 <genius1280@mail.ru>
2021-05-09 17:11:56 +0300

    Entity

---
+++ src/main/java/ru/tasklist/demo/entity/CategoryEntity.java
@@ -0,0 +1,47 @@
+package ru.tasklist.demo.entity;
+
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.Objects;
+
+@Entity
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@Table(name = "category", schema = "tasklist")
+public class CategoryEntity {
+    private Long id;
+    private String title;
+    private Long completedCount;
+    private Long uncompletedCount;
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "id")
+    public Long getId() {
+        return id;
+    }
+
+    @Basic
+    @Column(name = "title")
+    public String getTitle() {
+        return title;
+    }
+
+    @Basic
+    @Column(name = "completed_count")
+    public Long getCompletedCount() {
+        return completedCount;
+    }
+
+    @Basic
+    @Column(name = "uncompleted_count")
+    public Long getUncompletedCount() {
+        return uncompletedCount;
+    }
+
+}
